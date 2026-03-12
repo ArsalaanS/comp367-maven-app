d20f767d66cf856c9e83184df9fcfaff660580ab
Arsalaan Siddiqui <asidd101@my.centennialcollege.ca>
2026-03-12 18:57:35 -0400

    Initial Maven project with App.java

---
+++ src/main/java/com/centennial/App.java
@@ -0,0 +1,13 @@
+package com.centennial;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to COMP367" );
+    }
+}
